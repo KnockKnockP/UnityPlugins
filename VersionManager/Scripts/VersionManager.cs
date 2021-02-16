b67420284dd4236c7beb90b13d861ea39a24d618
KnockKnockP <memeboi420666@gmail.com>
2021-02-17 00:21:10 +0900

    Add a namespace

--- VersionManager/Scripts/VersionManager.cs
+++ VersionManager/Scripts/VersionManager.cs
@@ -4,31 +4,33 @@ using System.Globalization;
 using UnityEditor;
 using UnityEngine;
 
-public class VersionManager : MonoBehaviour {
-    [SerializeField] private GameObject[] MainBuildDisableObjects = null;
+namespace UnityPlugins {
+    public class VersionManager : MonoBehaviour {
+        [SerializeField] private GameObject[] MainBuildDisableObjects = null;
 
-    public void Awake() {
-        foreach (GameObject _gameObject in MainBuildDisableObjects) {
-            _gameObject.SetActive(Debug.isDebugBuild);
+        public void Awake() {
+            foreach (GameObject _gameObject in MainBuildDisableObjects) {
+                _gameObject.SetActive(Debug.isDebugBuild);
+            }
+            return;
         }
-        return;
-    }
-}
-
-[CustomEditor(typeof(VersionManager))]
-public class VersionManagerOverride : Editor {
-    public override void OnInspectorGUI() {
-        base.OnInspectorGUI();
-        if (GUILayout.Button("Generate new version number.") == true) {
-            GenerateNewVersionNumber();
-        }
-        return;
     }
 
-    private void GenerateNewVersionNumber() {
-        Debug.Log($"New version : {DateTime.Now.ToString("TESTBUILD-yyyy-MM-ddThh-mm-tt+0900", CultureInfo.InvariantCulture)}\r\n");
-        Debug.LogError("Do not forget to enter the password.");
-        return;
+    [CustomEditor(typeof(VersionManager))]
+    public class VersionManagerOverride : Editor {
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+            if (GUILayout.Button("Generate new version number.") == true) {
+                GenerateNewVersionNumber();
+            }
+            return;
+        }
+
+        private void GenerateNewVersionNumber() {
+            Debug.Log($"New version : {DateTime.Now.ToString("TESTBUILD-yyyy-MM-ddThh-mm-tt+0900", CultureInfo.InvariantCulture)}\r\n");
+            Debug.LogError("Do not forget to enter the password.");
+            return;
+        }
     }
 }
 #endif
