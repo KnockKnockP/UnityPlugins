f8ec3642ff2f8b8d53a9fadc1b8be71b3b6413ee
KnockKnockP <memeboi420666@gmail.com>
2021-02-21 23:41:23 +0900

    Group player utilities methods in to math and input structs.

--- PlayerUtilities/Scripts/PlayerUtilities.cs
+++ PlayerUtilities/Scripts/PlayerUtilities.cs
@@ -2,8 +2,32 @@ using UnityEngine;
 
 namespace UnityPlugins {
     public struct PlayerUtilities {
-        public static Vector2 MouseToWorld(Vector2 mousePosition, Camera camera) {
-            return camera.ScreenToWorldPoint(mousePosition);
+        public struct Math {
+            //Returns true if the distance between is longer than the maxDistance.
+            public static bool IsDiagnal(Vector2 position1, Vector2 position2) {
+                return ((position1.x != position2.x) && (position1.y != position2.y));
+            }
+
+            //Returns direction relative to position1.
+            public static Vector2 CalculateDirection(Vector2 position1, Vector2 position2) {
+                if (position1.x > position2.x) {
+                    return Vector2.left;
+                } else if (position2.x > position1.x) {
+                    return Vector2.right;
+                } else if (position1.y > position2.y) {
+                    return Vector2.down;
+                } else if (position2.y > position1.y) {
+                    return Vector2.up;
+                } else {
+                    return Vector2.zero;
+                }
+            }
+        }
+
+        public struct Input {
+            public static Vector2 MouseToWorld(Vector2 mousePosition, Camera camera) {
+                return camera.ScreenToWorldPoint(mousePosition);
+            }
         }
     }
 }
