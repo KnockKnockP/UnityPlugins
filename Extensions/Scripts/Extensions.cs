f2ee925d991b466b4ca6afb3507bbd1c74cb8b72
KnockKnockP <memeboi420666@gmail.com>
2021-03-28 21:07:16 +0900

    Add an extension.

--- Extensions/Scripts/Extensions.cs
+++ Extensions/Scripts/Extensions.cs
@@ -1,5 +1,6 @@
 using System;
-using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace UnityPlugins {
     public static class Extensions {
@@ -19,6 +20,46 @@ namespace UnityPlugins {
                 }
             }
             return newString;
+        }
+
+        public static ColorBlock GenerateButtonColors(this ColorBlock originalColorBlock, Color32 newColor) {
+            ColorBlock colorBlock = new ColorBlock {
+                normalColor = newColor,
+                highlightedColor = new Color32((byte)(PlayerUtilities.Math.Clamp((newColor.r + 10),
+                                                                                     byte.MinValue,
+                                                                                     byte.MaxValue)),
+                                                       (byte)(PlayerUtilities.Math.Clamp((newColor.g + 20),
+                                                                                     byte.MinValue,
+                                                                                     byte.MaxValue)),
+                                                       (byte)(PlayerUtilities.Math.Clamp((newColor.b + 10),
+                                                                                     byte.MinValue,
+                                                                                     byte.MaxValue)),
+                                                       byte.MaxValue),
+                pressedColor = new Color32((byte)(PlayerUtilities.Math.Clamp((newColor.r - 30),
+                                                                                     byte.MinValue,
+                                                                                     byte.MaxValue)),
+                                                   (byte)(PlayerUtilities.Math.Clamp((newColor.g - 30),
+                                                                                     byte.MinValue,
+                                                                                     byte.MaxValue)),
+                                                   (byte)(PlayerUtilities.Math.Clamp((newColor.b - 30),
+                                                                                     byte.MinValue,
+                                                                                     byte.MaxValue)),
+                                                   byte.MaxValue),
+                selectedColor = newColor,
+                disabledColor = new Color32((byte)(PlayerUtilities.Math.Clamp((newColor.r * 0.6f),
+                                                                                     byte.MinValue,
+                                                                                     byte.MaxValue)),
+                                                   (byte)(PlayerUtilities.Math.Clamp((newColor.g * 0.6f),
+                                                                                     byte.MinValue,
+                                                                                     byte.MaxValue)),
+                                                   (byte)(PlayerUtilities.Math.Clamp((newColor.b * 0.6f),
+                                                                                     byte.MinValue,
+                                                                                     byte.MaxValue)),
+                                                   byte.MaxValue),
+                colorMultiplier = originalColorBlock.colorMultiplier,
+                fadeDuration = originalColorBlock.fadeDuration
+            };
+            return colorBlock;
         }
     }
 }
