0273427de9a7c7276aa03388ebcedee14f08e901
KnockKnockP <memeboi420666@gmail.com>
2021-06-23 14:18:16 +0900

    Add more methods.

--- PlayerUtilities/Scripts/PlayerUtilities.cs
+++ PlayerUtilities/Scripts/PlayerUtilities.cs
@@ -1,15 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using UnityEditor;
 using UnityEngine;
 
 namespace UnityPlugins {
     public struct PlayerUtilities {
         public struct Math {
-            //Returns true if the distance between is longer than the maxDistance.
-            public static bool IsDiagonal(Vector2 position1, Vector2 position2) {
-                return ((position1.x != position2.x) && (position1.y != position2.y));
-            }
+            public static bool IsDiagonal(Vector2 position1, Vector2 position2) => ((position1.x != position2.x) && (position1.y != position2.y));
 
             public static T Clamp<T>(T value,
                                      T min,
@@ -22,7 +20,8 @@ namespace UnityPlugins {
                 return value;
             }
 
-            private static System.Random random = new System.Random(Guid.NewGuid().GetHashCode());
+            private static System.Random random = new System.Random(Guid.NewGuid()
+                                                                        .GetHashCode());
 
             public static T RandomEnum<T>(int? seed = null) where T : Enum {
                 if (seed != null) {
@@ -32,7 +31,14 @@ namespace UnityPlugins {
                 return (T)(values.GetValue(random.Next(values.Length)));
             }
 
-            //Start is inclusive, end is not.
+            /// <summary>
+            /// <paramref name="start"/> is inclusive, <paramref name="end"/> is not.
+            /// </summary>
+            /// <param name="start">Inclusive start.</param>
+            /// <param name="end">Exclusive end.</param>
+            /// <param name="seed">Optional seed parameter.</param>
+            /// <typeparam name="T">An enum type to randomly select from.</typeparam>
+            /// <returns>A random enum value.</returns>
             public static T RandomEnum<T>(int start,
                                           int end,
                                           int? seed = null) where T : Enum {
@@ -43,45 +49,84 @@ namespace UnityPlugins {
                 return (T)(values.GetValue(random.Next(start, end)));
             }
 
-            //Returns direction relative to position1.
+            /// <summary>
+            /// Calculates Vector2 direction.
+            /// </summary>
+            /// <param name="position1">Starting position.</param>
+            /// <param name="position2">Ending position.</param>
+            /// <returns>Returns direction relative to <paramref name="position1"/>.</returns>
             public static Vector2 CalculateDirection(Vector2 position1, Vector2 position2) {
-                if (position1.x > position2.x) {
-                    return Vector2.left;
-                } else if (position2.x > position1.x) {
-                    return Vector2.right;
-                } else if (position1.y > position2.y) {
-                    return Vector2.down;
-                } else if (position2.y > position1.y) {
-                    return Vector2.up;
-                } else {
-                    return Vector2.zero;
+                Vector2 subtracted = (position1 - position2),
+                        direction = Vector2.zero;
+                if (subtracted.x > 0f) {
+                    direction.x = -1f;
+                } else if (subtracted.x != 0f) {
+                    direction.x = 1f;
                 }
+
+                if (subtracted.y > 0f) {
+                    direction.y = -1f;
+                } else if (subtracted.y != 0f) {
+                    direction.y = 1f;
+                }
+
+                return direction;
+            }
+
+            /// <summary>
+            /// Calculates Vector3 direction.
+            /// </summary>
+            /// <param name="position1">Starting position.</param>
+            /// <param name="position2">Ending position.</param>
+            /// <returns>Returns distance relative to <paramref name="position1"/>.</returns>
+            public static Vector3 CalculateDirection(Vector3 position1, Vector3 position2) {
+                Vector3 subtracted = (position1 - position2),
+                        direction = Vector3.zero;
+                if (subtracted.x > 0f) {
+                    direction.x = -1f;
+                } else if (subtracted.x != 0f) {
+                    direction.x = 1f;
+                }
+
+                if (subtracted.y > 0f) {
+                    direction.y = -1f;
+                } else if (subtracted.y != 0f) {
+                    direction.y = 1f;
+                }
+
+                if (subtracted.z > 0f) {
+                    direction.z = -1f;
+                } else if (subtracted.z != 0f) {
+                    direction.z = 1f;
+                }
+
+                return direction;
             }
         }
 
         public struct Input {
-            public static int ParseInt(string input, int defaultReturn) {
-                return ((string.IsNullOrEmpty(input) == false) ? int.Parse(input) : defaultReturn);
+            public static int ParseInt(string input, int defaultReturn) => ((string.IsNullOrEmpty(input) == false) ? int.Parse(input) : defaultReturn);
+
+            public static long ParseLong(string input, long defaultReturn) => ((string.IsNullOrEmpty(input) == false) ? long.Parse(input) : defaultReturn);
+
+            public static RaycastHit MouseToHit(Camera camera) {
+                Ray mouseRay = camera.ScreenPointToRay(UnityEngine.Input.mousePosition);
+                Physics.Raycast(mouseRay,
+                                out RaycastHit raycastHit,
+                                1000f);
+                return raycastHit;
             }
 
-            public static long ParseLong(string input, long defaultReturn) {
-                return ((string.IsNullOrEmpty(input) == false) ? long.Parse(input) : defaultReturn);
-            }
-
-            public static Vector2 MouseToWorld(Vector2 mousePosition, Camera camera) {
-                return camera.ScreenToWorldPoint(mousePosition);
-            }
-
-            public static IEnumerable<RaycastHit2D> MouseToHits(Camera camera) {
-                return Physics2D.RaycastAll(MouseToWorld(UnityEngine.Input.mousePosition, camera), Vector2.zero);
-            }
+            public static IEnumerable<RaycastHit2D> MouseToHits2D(Camera camera) => Physics2D.RaycastAll(camera.ScreenToWorldPoint(UnityEngine.Input.mousePosition), Vector2.zero);
         }
 
         public struct Animation {
             public static void ChangeAnimation(Animator animator,
                                                string animationName,
                                                int index) {
-                if (animator.GetCurrentAnimatorStateInfo(index).IsName(animationName) == false) {
+                if (animator.GetCurrentAnimatorStateInfo(index)
+                            .IsName(animationName) ==
+                    false) {
                     animator.Play(animationName);
                 }
                 return;
@@ -89,15 +134,13 @@ namespace UnityPlugins {
         }
 
         public struct Other {
-            public static void PrintOutEnums<T>() {
-                PrintOutEnums(typeof(T));
-                return;
-            }
+            public static void PrintOutEnums<T>() => PrintOutEnums(typeof(T));
 
             public static void PrintOutEnums(Type enumType) {
                 foreach (string enumName in Enum.GetNames(enumType)) {
                     Debug.Log(enumName);
                 }
+                return;
             }
 
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
--- Tests/EditModeTests/PlayerUtilitiesTest.cs
+++ Tests/EditModeTests/PlayerUtilitiesTest.cs
@@ -44,10 +44,17 @@ namespace UnityPlugins {
 
                 [Test]
                 public static void TestCalculateDirection() {
+                    Assert.AreEqual(new Vector2(0f, 0f), PlayerUtilities.Math.CalculateDirection(new Vector2(-8.75f, 32.2f), new Vector2(-8.75f, 32.2f)));
                     Assert.AreEqual(new Vector2(0f, 1f), PlayerUtilities.Math.CalculateDirection(new Vector2(0f, 5.2f), new Vector2(0f, 10.2f)));
                     Assert.AreEqual(new Vector2(-1f, 0f), PlayerUtilities.Math.CalculateDirection(new Vector2(2.5f, 0f), new Vector2(-19.5f, 0f)));
                     Assert.AreEqual(new Vector2(0f, -1f), PlayerUtilities.Math.CalculateDirection(new Vector2(3.2f, -5.9f), new Vector2(3.2f, -8.9f)));
                     Assert.AreEqual(new Vector2(1f, 0f), PlayerUtilities.Math.CalculateDirection(new Vector2(-5.9f, -2.3f), new Vector2(9.2f, -2.3f)));
+                    
+                    Assert.AreEqual(new Vector3(0f, 0f, 0f), PlayerUtilities.Math.CalculateDirection(new Vector3(94.3f, -25.2f, 76.287f), new Vector3(94.3f, -25.2f, 76.287f)));
+                    Assert.AreEqual(new Vector3(0f, 1f, -1f), PlayerUtilities.Math.CalculateDirection(new Vector3(0f, 5.2f, 2f), new Vector3(0f, 10.2f, -1.4f)));
+                    Assert.AreEqual(new Vector3(-1f, 0f, 0f), PlayerUtilities.Math.CalculateDirection(new Vector3(2.5f, 0f, 2.5f), new Vector3(-19.5f, 0f, 2.5f)));
+                    Assert.AreEqual(new Vector3(0f, -1f, 1f), PlayerUtilities.Math.CalculateDirection(new Vector3(3.2f, -5.9f, 4.67f), new Vector3(3.2f, -8.9f, 4.7f)));
+                    Assert.AreEqual(new Vector3(1f, 0f, -1f), PlayerUtilities.Math.CalculateDirection(new Vector3(-5.9f, -2.3f, 9874.43f), new Vector3(9.2f, -2.3f, -38745.24f)));
                     return;
                 }
 
