3b2753d6af7307dd2d85b3b633e188708cd756b2
KnockKnockP <51238332+KnockKnockP@users.noreply.github.com>
2021-07-13 20:33:44 +0900

    Add monobehaviour singleton.

---
+++ MonoSingleton.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace UnityPlugins {
+    public class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T> {
+        private static T instance = null;
+
+        public static T Instance {
+            get {
+                EnsureSingleton();
+                return instance;
+            }
+        }
+
+        public static T FastInstance => instance;
+
+        private static void EnsureSingleton() {
+            if (instance == null) {
+                instance = FindObjectOfType<T>();
+
+                if (instance == null) {
+                    GameObject newGameObject = new GameObject {
+                        name = $"{typeof(T)} singleton"
+                    };
+
+                    instance = newGameObject.AddComponent<T>();
+                }
+            }
+
+            DontDestroyOnLoad(instance);
+            return;
+        }
+        
+        protected virtual void Awake() {
+            EnsureSingleton();
+            return;
+        }
+
+        protected virtual void OnDestroy() => instance = null;
+    }
+}
